fe7e47759abc35602f4d91049bfc5675302780d1
Tydecent <Smart100430@163.com>
2025-06-18 22:43:54 +0800

    Add files via upload

---
+++ zkdjs_pressure/zkdjs_pressure/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Timer1 = New Timer(components)
+        Label5 = New Label()
+        Label4 = New Label()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("幼圆", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(134))
+        Label1.Location = New Point(-1, -1)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(168, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "距语文开考" & vbCrLf
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("幼圆", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Label2.Location = New Point(-1, 35)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(49, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "剩余"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("DS-Digital", 36F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Red
+        Label3.Location = New Point(51, 26)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(268, 59)
+        Label3.TabIndex = 2
+        Label3.Text = "HHH:MM:SS"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 1
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("华文楷体", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(134))
+        Label5.ForeColor = Color.Red
+        Label5.Location = New Point(249, 2)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(37, 31)
+        Label5.TabIndex = 4
+        Label5.Text = "D"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("华文楷体", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(134))
+        Label4.ForeColor = Color.Black
+        Label4.Location = New Point(275, 2)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(43, 31)
+        Label4.TabIndex = 5
+        Label4.Text = "天"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("隶书", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(134))
+        Label6.Location = New Point(17, 75)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(269, 20)
+        Label6.TabIndex = 6
+        Label6.Text = "生命可以重来，中考只有一次"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(315, 99)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        FormBorderStyle = FormBorderStyle.None
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        ShowIcon = False
+        ShowInTaskbar = False
+        StartPosition = FormStartPosition.Manual
+        Text = "zkdjs"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+
+End Class
---
+++ zkdjs_pressure/zkdjs_pressure/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.TopMost = True
+        Timer1.Interval = 1 ' 每秒更新一次
+        Timer1.Start()
+        UpdateCountdown() ' 初始立即更新
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        UpdateCountdown()
+    End Sub
+
+    Private Sub UpdateCountdown()
+        Dim targetDate As New DateTime(2025, 6, 26, 9, 0, 0)
+        Dim remaining As TimeSpan = targetDate - DateTime.Now
+
+        ' 处理过期情况
+        If remaining.TotalMilliseconds <= 0 Then
+            Label3.Text = "000:00:00"
+            Return
+        End If
+
+        ' 计算总小时数（包含天数转换）
+        Dim totalHours As Integer = CInt(Math.Floor(remaining.TotalHours))
+        Dim hours As String = totalHours.ToString("D3") ' 3位小时数补零
+
+        ' 获取剩余分钟/秒（扣除已计算的小时部分）
+        Dim minutes As String = (remaining.Minutes).ToString("D2")
+        Dim seconds As String = (remaining.Seconds).ToString("D2")
+
+        Label3.Text = $"{hours}:{minutes}:{seconds}"
+
+        ' 更新Label5（天数倒计时）
+        Dim targetDay As New DateTime(2025, 6, 26)
+        Dim currentDate As DateTime = DateTime.Today
+        Dim daysRemaining As Integer = CInt((targetDay - currentDate).TotalDays)
+
+        ' 处理过期情况
+        If daysRemaining < 0 Then
+            daysRemaining = 0
+        End If
+
+        Label5.Text = daysRemaining.ToString()
+    End Sub
+End Class
